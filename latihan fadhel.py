9441fad60e8c023d1206a1cca930685d12346522
fadhelbaihaqi <54706274+fadhelbaihaqi@users.noreply.github.com>
2019-09-12 21:12:25 +0700

    tugas alpr

---
+++ latihan fadhel.py
@@ -0,0 +1,13 @@
+listGPA = [3,2.7,2.5,4]
+def gift(GPA):
+    nominal = 500000
+    return gift
+
+
+for GPA in listGPA :
+    if GPA > 3 :
+        print ("Anda mendapatkan gift berupa uang  ; Rp", gift(GPA)) 
+    else :
+        print ("tidak ada gift")   
+
+        
